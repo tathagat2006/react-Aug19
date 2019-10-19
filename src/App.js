001ada9a1312519320ba63de074bb1155012ae2f
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-10-19 18:10:02 +0530

    componentDidMount lifecycle
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios'
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      users: []
+    }
+  }
+
+  async componentWillMount() {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+    console.log(response)
+  }
+
+  render() {
+    return (
+      <div className='parent'>
+        <div>This is our homepage</div>
+        <div>Hello world!!</div>
+      </div >
+    )
+  }
 }
+
+// const App = () => (
+//   <div className="App">
+//     <header className="App-header">
+//       <img src={logo} className="App-logo" alt="logo" />
+//       <p>
+//         Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       <a
+//         className="App-link"
+//         href="https://reactjs.org"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//       >
+//         Learn React
+//         </a>
+//     </header>
+//   </div>
+// )
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
