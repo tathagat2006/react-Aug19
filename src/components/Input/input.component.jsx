d52bb108465e5a7598ee795681f0dc07be287713
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-10-20 23:08:23 +0530

    final commit
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- src/components/Input/input.component.jsx
+++ src/components/Input/input.component.jsx
@@ -10,23 +10,33 @@ class Input extends React.Component {
                 {
                     id: 0,
                     name: "item1",
+                    isDone: false
                 },
                 {
                     id: 1,
                     name: "item2",
+                    isDone: false
                 },
                 {
                     id: 2,
                     name: "item3",
+                    isDone: false
                 },
                 {
                     id: 3,
                     name: "item4",
+                    isDone: false
                 }
             ],
             userInput: ""
         }
     }
+
+    // setStateAsync = (state) => {
+    //     return new Promise((resolve) => {
+    //         this.setState(state, resolve)
+    //     });
+    // }
 
     handleDelete = (todo) => {
         let todolist = this.state.list.filter((item) => todo.id !== item.id)
@@ -45,10 +55,43 @@ class Input extends React.Component {
         this.setState({
             list: [...this.state.list, {
                 id: this.state.list.length - 1,
-                name: this.state.userInput
+                name: this.state.userInput,
+                isDone: false
             }]
         })
     }
+
+    handleToggle = async (todo) => {
+        let todolist = this.state.list.map(item => {
+            if (item.id === todo.id) {
+                item.isDone = !item.isDone
+                return item
+            }
+        })
+
+        this.setState(({}), state => ({
+            list: todolist
+        }))
+
+        // await this.setStateAsync({
+        //     list: todolist
+        // })
+    }
+
+    // asyncHandleToggle = async (todo) => {
+    //     await this.handleToggle(todo)
+    // }
+
+    // handleToggle = (todo) => {
+    //     // console.log('toggle')
+    //     this.state.list.map(item => {
+    //         if (item.id === todo.id) {
+    //             item.isDone = !item.isDone
+    //         }
+    //     })
+
+    //     console.log(this.state)
+    // }
 
     handleChange = (event) => {
         this.setState({
@@ -64,7 +107,7 @@ class Input extends React.Component {
                 </form>
                 <Button handleClick={this.handleClick} />
                 <ul className='list-group' style={{ margin: "3vh" }}>
-                    <List items={this.state.list} handleDelete={this.handleDelete} />
+                    <List items={this.state.list} handleDelete={this.handleDelete} handleToggle={this.handleToggle} />
                 </ul>
             </div>
         )
--- src/components/List/list.component.jsx
+++ src/components/List/list.component.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 // import Button from '../Button/button.component'
 
 const List = (props) => (
+
     <div>
+        {console.log(props)}
         {
             props.items.map(item => {
                 return (
                     <div>
-
-                        <li style={{ margin: "2vh", display: "inline-block", width: '50vh' }} className='list-group-item'>{item.name}</li>
+                        <li style={{ margin: "2vh", display: "inline-block", width: '50vh' }} onClick={() => props.handleToggle(item)} className={item.isDone ? "striked list-group-item" : "list-group-item"}>{item.name}</li>
                         <button className='btn btn-danger' onClick={() => props.handleDelete(item)}>Delete Item</button>
                     </div>
                 )
